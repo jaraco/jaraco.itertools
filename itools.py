c94ecf7f5b4edda95817bc1df4ee198e59a592a9
jaraco <none@none>
2005-01-25 16:50:06 +0000

    Added copyright notice, docstrings, and utf-8 encoding specifier.

--- itools.py
+++ itools.py
@@ -1,3 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+"""itools
+	Tools for working with iterables.  Complements itertools.
+	
+Copyright © 2004 Sandia National Laboratories  
+"""
+
+__author__ = 'Jason R. Coombs <jaraco@sandia.gov>'
+__version__ = '$Revision: 6 $a'[11:-2]
+__vssauthor__ = '$Author: Jaraco $'[9:-2]
+__date__ = '$Modtime: 04-06-23 12:24 $'[10:-2]
+
 import operator, itertools
 from tools import ordinalth
 
