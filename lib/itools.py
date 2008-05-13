dcdae40fea83334a71bacd2b8d862b3823737fda
jaraco <none@none>
2008-05-13 16:01:25 +0000

    Removed extraneous space per PEP8

--- lib/itools.py
+++ lib/itools.py
@@ -14,18 +14,18 @@ __date__ = '$Date$'[7:-2]
 import operator, itertools
 from tools import ordinalth
 
-class Count( object ):
+class Count(object):
 	"""
 	A stop object that will count how many times it's been called and return
 	False on the N+1st call.  Useful for use with takewhile.
-	>>> tuple( itertools.takewhile( Count( 5 ), xrange( 20 ) ) )
+	>>> tuple(itertools.takewhile(Count(5), xrange(20)))
 	(0, 1, 2, 3, 4)
 	"""
-	def __init__( self, limit ):
+	def __init__(self, limit):
 		self.count = 0
 		self.limit = limit
 		
-	def __call__( self, arg ):
+	def __call__(self, arg):
 		if not self.limit:
 			result = True
 		else:
@@ -35,45 +35,45 @@ class Count( object ):
 		self.count += 1
 		return result
 
-	def __str__( self ):
+	def __str__(self):
 		if limit:
 			return 'at most %d' % limit
 		else:
 			return 'all'
 
-class islice( object ):
+class islice(object):
 	"""May be applied to an iterable to limit the number of items returned.
 	Works similarly to count, except is called only once on an iterable.
 	Functionality is identical to islice, except for __str__ and reusability.
-	>>> tuple( islice( 5 ).apply( xrange( 20 ) ) )
+	>>> tuple(islice(5).apply(xrange(20)))
 	(0, 1, 2, 3, 4)
-	>>> tuple( islice( None ).apply( xrange( 20 ) ) )
+	>>> tuple(islice(None).apply(xrange(20)))
 	(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
 	"""
-	def __init__( self, *sliceArgs ):
+	def __init__(self, *sliceArgs):
 		self.sliceArgs = sliceArgs
 
-	def apply( self, i ):
-		return itertools.islice( i, *self.sliceArgs )
+	def apply(self, i):
+		return itertools.islice(i, *self.sliceArgs)
 
-	def __str__( self ):
-		if self.sliceArgs == ( None, ):
+	def __str__(self):
+		if self.sliceArgs == (None,):
 			result = 'all'
 		else:
 			result = self._formatArgs()
 		return result
 
-	def _formatArgs( self ):
+	def _formatArgs(self):
 		baseOneRange = lambda (a,b): '%d to %d' % (a+1,b)
-		if len( self.sliceArgs ) == 1:
+		if len(self.sliceArgs) == 1:
 			result = 'at most %d' % self.sliceArgs
-		if len( self.sliceArgs ) == 2:
-			result = 'items %s' % baseOneRange( self.sliceArgs )
-		if len( self.sliceArgs ) == 3:
-			result = 'every %s item from %s' % ( ordinalth( self.sliceArgs[2] ), baseOneRange( self.sliceArgs[0:2] ) )
+		if len(self.sliceArgs) == 2:
+			result = 'items %s' % baseOneRange(self.sliceArgs)
+		if len(self.sliceArgs) == 3:
+			result = 'every %s item from %s' % (ordinalth(self.sliceArgs[2]), baseOneRange(self.sliceArgs[0:2]))
 		return result
 	
-class LessThanNBlanks( object ):
+class LessThanNBlanks(object):
 	"""
 	An object that when called will return True until n false elements
 	are encountered.
@@ -81,25 +81,25 @@ class LessThanNBlanks( object ):
 	Can be used with filter or itertools.ifilter, for example:
 
 >>> import itertools
->>> sampleData = ['string 1', 'string 2', '', 'string 3', '', 'string 4', '', '', 'string 5' ]
->>> first = itertools.takewhile( LessThanNBlanks( 2 ), sampleData )
->>> tuple( first )
+>>> sampleData = ['string 1', 'string 2', '', 'string 3', '', 'string 4', '', '', 'string 5']
+>>> first = itertools.takewhile(LessThanNBlanks(2), sampleData)
+>>> tuple(first)
 ('string 1', 'string 2', '', 'string 3')
->>> first = itertools.takewhile( LessThanNBlanks( 3 ), sampleData )
->>> tuple( first )
+>>> first = itertools.takewhile(LessThanNBlanks(3), sampleData)
+>>> tuple(first)
 ('string 1', 'string 2', '', 'string 3', '', 'string 4')
 	"""
-	def __init__( self, nBlanks ):
+	def __init__(self, nBlanks):
 		self.limit = nBlanks
 		self.count = 0
 
-	def __call__( self, arg ):
+	def __call__(self, arg):
 		self.count += not arg
 		if self.count > self.limit:
 			raise ValueError, "Should not call this object anymore."
 		return self.count < self.limit
 
-class LessThanNConsecutiveBlanks( object ):
+class LessThanNConsecutiveBlanks(object):
 	"""
 	An object that when called will return True until n consecutive
 	false elements are encountered.
@@ -107,109 +107,109 @@ class LessThanNConsecutiveBlanks( object ):
 	Can be used with filter or itertools.ifilter, for example:
 
 >>> import itertools
->>> sampleData = ['string 1', 'string 2', '', 'string 3', '', 'string 4', '', '', 'string 5' ]
->>> first = itertools.takewhile( LessThanNConsecutiveBlanks( 2 ), sampleData )
->>> tuple( first )
+>>> sampleData = ['string 1', 'string 2', '', 'string 3', '', 'string 4', '', '', 'string 5']
+>>> first = itertools.takewhile(LessThanNConsecutiveBlanks(2), sampleData)
+>>> tuple(first)
 ('string 1', 'string 2', '', 'string 3', '', 'string 4', '')
 	"""
 	
-	def __init__( self, nBlanks ):
+	def __init__(self, nBlanks):
 		self.limit = nBlanks
 		self.count = 0
 		self.last = False
 		
-	def __call__( self, arg ):
+	def __call__(self, arg):
 		self.count += not arg
 		if arg:
 			self.count = 0
-		self.last = operator.truth( arg )
+		self.last = operator.truth(arg)
 		if self.count > self.limit:
 			raise ValueError, "Should not call this object anymore."
 		return self.count < self.limit
 
-class splitter( object ):
+class splitter(object):
 	"""object that will split a string with the given arguments for each call
-	>>> s = splitter( ',' )
-	>>> list( s( 'hello, world, this is your, master calling' ) )
+	>>> s = splitter(',')
+	>>> list(s('hello, world, this is your, master calling'))
 	['hello', ' world', ' this is your', ' master calling']
 """
-	def __init__( self, sep = None ):
+	def __init__(self, sep = None):
 		self.sep = sep
 
-	def __call__( self, s ):
+	def __call__(self, s):
 		lastIndex = 0
 		while True:
-			nextIndex = s.find( self.sep, lastIndex )
+			nextIndex = s.find(self.sep, lastIndex)
 			if nextIndex != -1:
-				yield s[ lastIndex:nextIndex ]
+				yield s[lastIndex:nextIndex]
 				lastIndex = nextIndex + 1
 			else:
-				yield s[ lastIndex: ]
+				yield s[lastIndex:]
 				break
 
-def chunkGenerator( seq, size ):
+def chunkGenerator(seq, size):
 	"""returns sequence or iterable seq in chunks of size
-	>>> c = chunkGenerator( xrange( 11 ), 3 )
-	>>> tuple( c )
+	>>> c = chunkGenerator(xrange(11), 3)
+	>>> tuple(c)
 	((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10))
-	>>> c = chunkGenerator( range( 10 ), 3 )
-	>>> tuple( c )
+	>>> c = chunkGenerator(range(10), 3)
+	>>> tuple(c)
 	((0, 1, 2), (3, 4, 5), (6, 7, 8), (9,))
 	"""
-	if isinstance( seq, basestring ):
+	if isinstance(seq, basestring):
 		raise TypeError, 'Cannot use chunkGenerator on strings.  Use tools.chunkGenerator instead'
 	# make sure sequence is iterable
-	seq = iter( seq )
+	seq = iter(seq)
 	while 1:
-		result = tuple( itertools.islice( seq, size ) )
+		result = tuple(itertools.islice(seq, size))
 		if not result: break
 		yield result
 
-def adjacentPairs( i ):
+def adjacentPairs(i):
 	"""Yield adjacent pairs of a single iterable as pairs
-	>>> tuple( adjacentPairs( iter( xrange( 5 ) ) ))
+	>>> tuple(adjacentPairs(iter(xrange(5))))
 	((0, 1), (1, 2), (2, 3), (3, 4))
 	"""
 	last = i.next()
 	while True:
 		next = i.next()
-		yield ( last, next )
+		yield (last, next)
 		last = next
 
-def chain( sequences ):
+def chain(sequences):
 	"""functions like itertools.chain, except chains everything in sequences.
-	Equivalent to itertools.chain( *sequences ) except sequences is evaluated
+	Equivalent to itertools.chain(*sequences) except sequences is evaluated
 	on demand."""
 	for sequence in sequences:
 		for item in sequence:
 			yield item
 
-def infiniteCall( f, *args ):
+def infiniteCall(f, *args):
 	"Perpetually yield the result of calling function f."
 	while True:
-		yield f( *args )
+		yield f(*args)
 
-def evalAll( i ):
+def evalAll(i):
 	"Cause an iterable to evaluate all of its arguments, but don't store any result."
 	for x in i: pass
 
-def evalN( i, n ):
+def evalN(i, n):
 	"Cause an iterable to evaluate n of its arguments, but don't store any result."
-	for x in itertools.islice( i, n ): pass
+	for x in itertools.islice(i, n): pass
 	
-class Counter( object ):
-	def __init__( self, i ):
+class Counter(object):
+	def __init__(self, i):
 		self.__count__ = 0
-		self.__i__ = enumerate( i )
+		self.__i__ = enumerate(i)
 
-	def __iter__( self ): return self
+	def __iter__(self): return self
 
-	def next( self ):
+	def next(self):
 		index, result = self.__i__.next()
 		self.__count__ = index + 1
 		return result
 
-	def GetCount( self ):
+	def GetCount(self):
 		return self.__count__
 
 # todo, factor out caching capability
@@ -250,11 +250,11 @@ def flatten(subject, test=None):
 	"""flatten an iterable with possible nested iterables.
 	Adapted from
 	http://mail.python.org/pipermail/python-list/2003-November/233971.html
-	>>> flatten( ['a','b',['c','d',['e','f'],'g'],'h'] ) == ['a','b','c','d','e','f','g','h']
+	>>> flatten(['a','b',['c','d',['e','f'],'g'],'h']) == ['a','b','c','d','e','f','g','h']
 	True
 
 	Note this will normally ignore string types as iterables.
-	>>> flatten( ['ab', 'c'] )
+	>>> flatten(['ab', 'c'])
 	['ab', 'c']
 	"""
 	return list(iflatten(subject, test))
